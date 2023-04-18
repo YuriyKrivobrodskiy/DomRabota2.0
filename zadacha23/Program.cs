0337b731a48f573ec59c0e44cbaa6e0b03fa8d7c
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-18 12:49:35 +0300

    zadacha23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,23 @@
+﻿// программа,которая принимает на вход число (N) и выдаёт таблицу кубов числа N
+
+int N = ReadInt("Введите число: ");
+System.Console.WriteLine(string.Join(", ", Square(N)));
+int ReadInt(string argument)
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(),out i))
+    {
+        Console.WriteLine("Это не число!");
+    }
+    return i;
+}
+int [] Square( int N )
+{
+    int [] S = new int[N];
+    for ( int i = 0; i < N; i++ )
+    {
+        S[i] = (int)Math.Pow( i, 3 );
+    }
+    return S;
+}
