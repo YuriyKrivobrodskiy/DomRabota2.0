3d761c132119fb41dff09513c668e2950239ba2f
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-18 15:07:27 +0300

    zadacha19

---
+++ zadacha19/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом/
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+if ( len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"Число {number} - палиндром.");
+    }
+    else
+    {
+    Console.WriteLine($"Число {number} - не палиндром.");
+    }
+}
+else 
+{
+    Console.WriteLine("Вы ввели не корректное число. Введите пятизначное число.");
+}    
